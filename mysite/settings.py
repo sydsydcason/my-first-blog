1c6395eaa853e090179b8d1c1ce186101881d4f4
sydsydcason <scason@ywlcs.org>
2018-02-03 11:06:39 -0600

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'g=nl@1m8rotlvm!hlz0q+o-gosdjjlpwz#g0_769*rtl#k@xu+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '<your_username>.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'sydcason.pythonanywhere.com']
 
 
 # Application definition
